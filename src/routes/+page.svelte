365669756e8d11f96dd83b7bad1b7912c8e5f1fb
kimdonggu42 <kdonggoo44@gmail.com>
2023-10-13 20:15:33 +0900

    style: Reservation List 페이지 퍼블리싱

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import addIcon from '../lib/icons/add.svg';
+	import closeIcon from '../lib/icons/close.svg';
+</script>
+
+<div class="w-[800px] h-[600px] border border-solid border-[#848484] rounded-xl">
+	<header class="flex items-center h-[75px] bg-white rounded-t-xl">
+		<div class="basis-[30%] pl-[20px]">
+			<button
+				class="flex items-center gap-x-[5px] px-[20px] py-[15px] bg-[#f7f7f5] rounded-lg shadow-lg"
+				><img src={addIcon} alt="add_icon" />
+				<div class="text-[#ea541d]">New Reservation</div>
+			</button>
+		</div>
+		<div class="flex justify-center basis-[40%]">
+			<div class="text-[25px] text-[#6b6460]">Reservation</div>
+		</div>
+		<div class="flex items-center justify-end basis-[30%] pr-[20px] text-righ">
+			<button>
+				<img src={closeIcon} alt="close_icon" />
+			</button>
+		</div>
+	</header>
+	<div class="bg-[#f5f5f3] h-[calc(100%-75px)] rounded-b-xl" />
+</div>
