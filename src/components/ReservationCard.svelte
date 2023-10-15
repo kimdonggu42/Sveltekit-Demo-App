5cfd12d3db36d320657e211cb2763d72fc8960e7
kimdonggu42 <kdonggoo44@gmail.com>
2023-10-15 16:03:54 +0900

    feat: seated 버튼 클릭 시 리스트에서 사라지는 기능 추가

--- src/components/ReservationCard.svelte
+++ src/components/ReservationCard.svelte
@@ -20,48 +20,64 @@
 		}
 	};
 
+	const handlePatchSeated = async (reservationId, seatedState) => {
+		const newSeated = {
+			seated: seatedState ? false : true
+		};
+		await reservationsStore.patchReservation(reservationId, newSeated);
+	};
+
 	const handleDeleteReservation = async (reservationId) => {
 		await reservationsStore.deleteReservation(reservationId);
 	};
 </script>
 
-<li class="flex flex-col h-fit gap-y-[15px] p-[15px] bg-white rounded-xl shadow-lg">
-	<div class="flex items-center gap-x-[10px]">
-		<div>{reservation.name}</div>
-		<div class="flex items-center gap-x-[2px] px-[10px] py-[5px] shadow-lg bg-[#f7f7f7] rounded-xl">
-			<img src={phoneIcon} alt="phone_icon" />
-			<div class="text-[14px] text-[#6b6460]">{reservation.phone}</div>
+{#if !reservation.seated}
+	<li class="flex flex-col h-fit gap-y-[15px] p-[15px] bg-white rounded-xl shadow-lg">
+		<div class="flex items-center gap-x-[10px]">
+			<div>{reservation.name}</div>
+			<div
+				class="flex items-center gap-x-[2px] px-[10px] py-[5px] shadow-lg bg-[#f7f7f7] rounded-xl"
+			>
+				<img src={phoneIcon} alt="phone_icon" />
+				<div class="text-[14px] text-[#6b6460]">{reservation.phone}</div>
+			</div>
 		</div>
-	</div>
-	<div class="flex items-center gap-x-[7px]">
-		<img src={eventIcon} alt="event_icon" />
-		<div>
-			{reservation.date}, {renameTime(reservation.hour)}:{renameTime(reservation.minute)}
-			{reservation.meridiem}
+		<div class="flex items-center gap-x-[7px]">
+			<img src={eventIcon} alt="event_icon" />
+			<div>
+				{reservation.date}, {renameTime(reservation.hour)}:{renameTime(reservation.minute)}
+				{reservation.meridiem}
+			</div>
 		</div>
-	</div>
-	<div class="flex items-center gap-x-[7px]">
-		<img src={groupIcon} alt="group_icon" />
-		<div>{reservation.guests}</div>
-	</div>
-	<div class="flex gap-x-[7px]">
-		<div class="text-[#6b6460]">Reserved Table</div>
-		<ul>
-			{#each reservation.table as table}
-				<li>{renameTable(table)}</li>
-			{/each}
-		</ul>
-	</div>
-	<div class="flex items-center gap-x-[7px]">
-		<div>{reservation.note}</div>
-		<img src={editIcon} alt="edit_icon" />
-	</div>
-	<div class="flex items-center gap-x-[10px]">
-		<button
-			class="p-[15px] bg-[#f7f7f5] rounded-lg shadow-lg"
-			on:click={handleDeleteReservation(reservation.id)}
-			><img src={trashIcon} alt="trash_icon" /></button
-		>
-		<button class="w-[140px] text-white py-[15px] bg-primary rounded-lg shadow-lg">Seated</button>
-	</div>
-</li>
+		<div class="flex items-center gap-x-[7px]">
+			<img src={groupIcon} alt="group_icon" />
+			<div>{reservation.guests}</div>
+		</div>
+		<div class="flex gap-x-[7px]">
+			<div class="text-[#6b6460]">Reserved Table</div>
+			<ul>
+				{#each reservation.table as table}
+					<li>{renameTable(table)}</li>
+				{/each}
+			</ul>
+		</div>
+		<div class="flex items-center gap-x-[7px]">
+			<div>{reservation.note}</div>
+			<img src={editIcon} alt="edit_icon" />
+		</div>
+		<div class="flex items-center gap-x-[10px]">
+			<button
+				class="p-[15px] bg-[#f7f7f5] rounded-lg shadow-lg"
+				on:click={handleDeleteReservation(reservation.id)}
+				><img src={trashIcon} alt="trash_icon" /></button
+			>
+			<button
+				class="w-[140px] text-white py-[15px] bg-primary rounded-lg shadow-lg"
+				on:click={handlePatchSeated(reservation.id, reservation.seated)}
+			>
+				Seated
+			</button>
+		</div>
+	</li>
+{/if}
--- src/stores/reservations.js
+++ src/stores/reservations.js
@@ -10,24 +10,32 @@ const createReservationsStore = () => {
       const reservationsData = res.data;
       set(reservationsData);
     } catch (error) {
-      console.error("예약 정보를 가져오는 중 오류가 발생했습니다:", error);
+      console.error("예약 정보를 가져오는 중 오류가 발생했습니다.", error);
     }
   };
   loadReservations();
+
+  const patchReservation = async (reservationId, updateSeated) => {
+    try {
+      await axios.patch(`http://localhost:3001/reservation/${reservationId}`, updateSeated);
+      await loadReservations();
+    } catch (error) {
+      console.error("예약 중 오류가 발생했습니다.", error);
+    }
+  };
 
   const deleteReservation = async (reservationId) => {
     try {
       await axios.delete(`http://localhost:3001/reservation/${reservationId}`);
       await loadReservations();
     } catch (error) {
-      console.error("예약 삭제 중 오류가 발생했습니다:", error);
+      console.error("예약 삭제 중 오류가 발생했습니다.", error);
     }
   };
 
-
-
   return {
     subscribe,
+    patchReservation,
     deleteReservation
   };
 };
